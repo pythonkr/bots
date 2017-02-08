b9369bb0fb9352111c1d566cbf5f0559d55fe0ea
HueyPark <jaewan.huey.park@gmail.com>
2017-02-08 22:09:18 +0900

    Fix: the froblem that deadline object can't be created

--- deadline_bot/deadline_bot.py
+++ deadline_bot/deadline_bot.py
@@ -9,15 +9,11 @@ for deadline in get_deadlines():
     try:
         message = ''
         if deadline.remain_day > 0:
-            message = '```{}``` {} 님 작업 마감이 {}일 남았습니다'.format(deadline.work,
-                    deadline.worker,
-                    deadline.remain_day)
+            message = '```{}``` {} 님 작업 마감이 {}일 남았습니다'.format(deadline.work, deadline.worker, deadline.remain_day)
         elif deadline.remain_day == 0:
             message = '```{}``` {} 님 작업 마감일입니다'.format(deadline.work, deadline.worker)
         else:
-            message = '```{}``` {} 님 작업 마감이 {}일 지났습니다'.format(deadline.work,
-                    deadline.worker
-                    -deadline.remain_day)
+            message = '```{}``` {} 님 작업 마감이 {}일 지났습니다'.format(deadline.work, deadline.worker, -deadline.remain_day)
         slacker.chat.post_message('#' + deadline.channel, message, as_user=True)
     except Exception as e:
         logger.warn('Error: {}'.format(e))
--- deadline_bot/sheet/deadline.py
+++ deadline_bot/sheet/deadline.py
@@ -1,9 +1,9 @@
 class Deadline:
-    def __init__(self, remain_day, work, channel):
+    def __init__(self, remain_day, work, channel, worker):
         self.remain_day = remain_day
         self.work = work
         self.channel = channel
+        self.worker = worker
 
     def __str__(self):
-        return 'remain_day: {}, work: {}, channel: {}'.format(self.remain_day, self.work, self.channel)
-
+        return 'remain_day: {}, work: {}, channel: {}, worker: {}'.format(self.remain_day, self.work, self.channel, self.worker)
