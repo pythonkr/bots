e9da6a22749977dbe46bddf05ea0258bf9da4618
HueyPark <jaewan.huey.park@gmail.com>
2017-02-22 23:51:38 +0900

    Create: .env config file

---
+++ deadline_bot/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+GOOGLE_API_CREDENTIAL = './google_api_credential.json'
+SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
