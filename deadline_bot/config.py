72491e7cecd4edfaa1235a3bc16bb4c60d81f40d
HueyPark <jaewan.huey.park@gmail.com>
2017-02-23 05:06:46 +0900

    Update: env GOOLE_API_CREDENTIAL use absolute path

--- deadline_bot/config.py
+++ deadline_bot/config.py
@@ -5,5 +5,5 @@ from os.path import join, dirname
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-GOOGLE_API_CREDENTIAL = './google_api_credential.json'
+GOOGLE_API_CREDENTIAL = os.path.dirname(os.path.abspath(__file__)) + '/google_api_credential.json'
 SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
