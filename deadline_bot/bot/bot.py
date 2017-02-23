201fcdc6c32be9c3fa525d71a9ad55e96fa4e2d1
HueyPark <jaewan.huey.park@gmail.com>
2017-02-23 21:23:27 +0900

    Update: message has a target on weekdays only

--- deadline_bot/bot/bot.py
+++ deadline_bot/bot/bot.py
@@ -1,4 +1,5 @@
 from config import SLACK_BOT_TOKEN
+from datetime import datetime
 from logger import logger
 from sheet.deadline import Deadline
 from slacker import Slacker
@@ -21,12 +22,20 @@ class Bot:
 
     def __post_message(self, channel, worker, remain_day, work):
         try:
-            message = '{} 님 {} 입니다 : {}'.format(worker, self.__get_remain_day_str(remain_day), work)
+            message = self.__generate_message(worker, remain_day, work)
 
             full_channel = self.__get_full_channel(channel)
             self.__slacker.chat.post_message(full_channel, message, as_user=True, link_names=1)
         except Exception as e:
             logger.warn('error: {}, channel: {}, message: {}'.format(e, channel, message))
+
+    def __generate_message(self, worker, remain_day, work):
+        message = '{} 입니다 : {}'.format(self.__get_remain_day_str(remain_day), work)
+
+        if datetime.today().weekday() <= 4:
+            message = '{} 님 {}'.format(worker, message)
+
+        return message
 
     def __get_remain_day_str(self, remain_day: int):
         if remain_day == 0:
