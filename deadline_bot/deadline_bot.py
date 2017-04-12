a32faab65b9cd1e71395482874e841756aeabf03
HueyPark <jaewan.huey.park@gmail.com>
2017-04-12 22:16:22 +0900

    Create scheduler

--- deadline_bot/deadline_bot.py
+++ deadline_bot/deadline_bot.py
@@ -1,7 +1,12 @@
 from bot import Bot
+import schedule
 from sheet.sheet import get_deadlines
 
-deadlines = get_deadlines()
 
-bot = Bot()
-bot.post_message(deadlines)
+def job():
+    deadlines = get_deadlines()
+
+    bot = Bot()
+    bot.post_message(deadlines)
+
+schedule.every().day.at('01:00').do(job)
