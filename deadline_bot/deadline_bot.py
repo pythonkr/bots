845f608b861ad86dd11e901f90c4f7cc6a87441b
HueyPark <jaewan.huey.park@gmail.com>
2017-04-13 17:02:43 +0900

    Add schedule run loop

--- deadline_bot/deadline_bot.py
+++ deadline_bot/deadline_bot.py
@@ -1,5 +1,6 @@
-from bot import Bot
 import schedule
+import time
+from bot import Bot
 from sheet.sheet import get_deadlines
 
 
@@ -10,3 +11,7 @@ def job():
     bot.post_message(deadlines)
 
 schedule.every().day.at('01:00').do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
